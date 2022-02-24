2725c9f13f5fa19544333bbe769fe7c2525c5683
chrilona <christabelalona@gmail.com>
2022-02-24 17:54:11 +0300

    re-do return function

--- src/LONA.KT.kt
+++ src/LONA.KT.kt
@@ -1,4 +1,28 @@
 fun main(){
-    var x =("lona")
+    var x = myName("lona","atieno")
     println(x)
+    var o = product(77,77)
+    println(o)
+    var wg = weight("68kgs")
+    println(wg)
+    var n = whatSheDoes("Front-End Mobile Development ")
+    println(n)
+}
+fun myName(firstName:String,otherName:String):String{
+
+    var z= otherName
+    return z
+}
+ fun product(num:Int,num1:Int):Int{
+
+     var y=num*num1
+     return y
+ }
+fun weight(w:String):String{
+    var t = w
+    return t
+}
+fun whatSheDoes(s:String):String{
+    var r = s
+    return r
 }
